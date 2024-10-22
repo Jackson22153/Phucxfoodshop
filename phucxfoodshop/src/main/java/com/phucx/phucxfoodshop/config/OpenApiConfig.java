84fd9d7cc9ac687f1a604d104dbbba1b8e35539d
Phucx <phucxauxi929@gmail.com>
2024-10-22 13:42:13 +0700

    basci authentication for swagger

--- phucxfoodshop/src/main/java/com/phucx/phucxfoodshop/config/OpenApiConfig.java
+++ phucxfoodshop/src/main/java/com/phucx/phucxfoodshop/config/OpenApiConfig.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
@@ -28,6 +31,13 @@ public class OpenApiConfig {
 
         Info info = new Info();
         info.description("Phucxfoodshop API").contact(contact).version("1.0.0").title("Phucxfoodshop API");
-        return new OpenAPI().info(info).servers(List.of(serverUrl));
+        return new OpenAPI().info(info)
+            .servers(List.of(serverUrl))
+            .components(
+                new Components()
+                    .addSecuritySchemes("basicAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("basic")))
+            .security(List.of(new SecurityRequirement().addList("basicAuth")));
     }
 }
